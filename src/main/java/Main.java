6fdae03f1469d770dbb87d77efa35a7ee0857a30
Anthony F <anthony.femenia@hotmail.fr>
2023-12-08 22:53:59 +0100

    pass 2nd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,37 @@ public class Main {
                 System.out.println("Received data");
 
                 final byte[] bufResponse = new byte[512];
+                // Packet Identifer
+                bufResponse[0] = buf[0];
+                bufResponse[1] = buf[1];
+                // QuestoinResponse
+                bufResponse[2] |= (byte) 0b10000000;
+                // OPCODE
+                bufResponse[2] &= (byte) 0b10000111;
+                // Authoritative Answer
+                bufResponse[2] &= (byte) 0b11111011;
+                // Truncation
+                bufResponse[2] &= (byte) 0b11111101;
+                // Recursion Desired
+                bufResponse[2] &= (byte) 0b11111110;
+                // Recursion Available
+                bufResponse[3] &= (byte) 0b01111111;
+                // Reserved
+                bufResponse[3] &= (byte) 0b10001111;
+                // Error
+                bufResponse[3] &= (byte) 0b11110000;
+                // Question Count
+                bufResponse[4] = 0;
+                bufResponse[5] = 0;
+                // Answer Record Count
+                bufResponse[6] = 0;
+                bufResponse[7] = 0;
+                // Authority Record Count
+                bufResponse[8] = 0;
+                bufResponse[9] = 0;
+                // Additional Record Count
+                bufResponse[10] = 0;
+                bufResponse[11] = 0;
                 final DatagramPacket packetResponse = new DatagramPacket(bufResponse, bufResponse.length, packet.getSocketAddress());
                 serverSocket.send(packetResponse);
             }

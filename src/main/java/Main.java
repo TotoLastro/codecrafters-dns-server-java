04ca0b09ee9e56dd24acee747a92c322396b9342
Anthony F <anthony.femenia@hotmail.fr>
2023-12-15 00:18:32 +0100

    pass 8th stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,16 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        InetSocketAddress forwardAddress = retrieveForwardAddress(args);
         try (DatagramSocket serverSocket = new DatagramSocket(2053)) {
+            serverSocket.setReuseAddress(true);
             while(true) {
                 final byte[] buf = new byte[512];
                 final DatagramPacket packet = new DatagramPacket(buf, buf.length);
@@ -13,7 +18,14 @@ public class Main {
 
                 final DNSMessage questionMessage = DNSMessageDecoder.decode(buf);
                 System.out.println("Received data : " + questionMessage);
-                final DNSMessage responseMessage = getResponseMessage(questionMessage);
+
+                final DNSMessage responseMessage;
+                if (forwardAddress != null) {
+                    responseMessage = getResponseMessageFromForwardServer(questionMessage, serverSocket, forwardAddress);
+                } else {
+                    responseMessage = getResponseMessage(questionMessage);
+                }
+
                 System.out.println("Response data : " + responseMessage);
                 byte[] bufResponse = DNSMessageEncoder.encode(responseMessage);
 
@@ -23,6 +35,49 @@ public class Main {
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
+    }
+
+    private static InetSocketAddress retrieveForwardAddress(String[] args) {
+        Map<String, String> arguments = parseArgs(args);
+        if (arguments.containsKey("--resolver")) {
+            String[] resolverAddress = arguments.get("--resolver").split(":");
+            return new InetSocketAddress(
+                resolverAddress[0],
+                Integer.parseInt(resolverAddress[1])
+            );
+        }
+        return null;
+    }
+
+    private static Map<String, String> parseArgs(String[] args) {
+        Map<String, String> arguments = new HashMap<>();
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].startsWith("-")) {
+                arguments.put(args[i], args[i + 1]);
+                i++;
+            } else {
+                arguments.put(args[i], "");
+            }
+        }
+        return arguments;
+    }
+
+    private static DNSMessage getResponseMessageFromForwardServer(
+        DNSMessage questionMessage,
+        DatagramSocket serverSocket,
+        InetSocketAddress forwardAddress
+    ) throws IOException {
+        byte[] question = DNSMessageEncoder.encode(questionMessage);
+        DatagramPacket questionPacket = new DatagramPacket(question, question.length);
+        serverSocket.connect(forwardAddress);
+        serverSocket.send(questionPacket);
+        serverSocket.disconnect();
+
+        final byte[] buf = new byte[512];
+        final DatagramPacket responsePacket = new DatagramPacket(buf, buf.length);
+        serverSocket.receive(responsePacket);
+
+        return DNSMessageDecoder.decode(buf);
     }
 
     private static DNSMessage getResponseMessage(DNSMessage questionMessage) {
